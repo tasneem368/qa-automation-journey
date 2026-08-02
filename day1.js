b3c0e57af291177f684da55ccd01500e7bb26e95
tasneem368 <78647188+tasneem368@users.noreply.github.com>
2026-08-02 22:42:10 +0000

    variable

---
+++ day1.js
@@ -0,0 +1,6 @@
+// variable let, var and constant   day1.js in your Codespace and write:
+
+let ownerName = 'tasneem';
+const date = '30/7/2026';
+let progressDate;
+ console.log(`my name is ${ownerName}  i was born in ${date}`);
