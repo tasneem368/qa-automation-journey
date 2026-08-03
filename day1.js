b9f403c3a47a02cd0a0b0cc8c12fd7c4cb26b6fe
tasneem368 <78647188+tasneem368@users.noreply.github.com>
2026-08-03 21:17:53 +0000

    An array of 5 QA tools you want to learn

--- day1.js
+++ day1.js
@@ -4,3 +4,8 @@ let ownerName = 'tasneem';
 const date = '30/7/2026';
 let progressDate;
  console.log(`my name is ${ownerName}  i was born in ${date}`);
+
+ // - An array of 5 QA tools you want to learn
+
+ let  neededSkils ='[automtionplaywright, cypress, appium, docker, agentic ]';
+ console.log( `the skills that i should learn is ${neededSkils}`);  
