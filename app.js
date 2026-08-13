af295060bb95e9c82582d85989c9e6d241f44a6a
tasneem368 <78647188+tasneem368@users.noreply.github.com>
2026-08-13 17:32:34 +0000

    app mini project

---
+++ app.js
@@ -0,0 +1,10 @@
+let usd = 1200;
+let aud = usd * 1.5;
+console.log(`USD: ${usd}, AUD: ${aud}`);
+
+// comparison operators 
+let comparison1 = "5" == 5; // true
+let comparison2 = "5" === 5; // false
+let comparison3 = 5 != "5"; // false
+let comparison4 = 5 !== "5"; // true
+console.log(`Comparison 1: ${comparison1}, Comparison 2: ${comparison2}, Comparison 3: ${comparison3}, Comparison 4: ${comparison4}`);
