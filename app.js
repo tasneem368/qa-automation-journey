465be5038204739abe5d2003998bfef51d118f27
tasneem368 <78647188+tasneem368@users.noreply.github.com>
2026-08-16 15:44:55 +0000

    first task to recap

--- app.js
+++ app.js
@@ -1,10 +1,24 @@
-let usd = 1200;
-let aud = usd * 1.5;
-console.log(`USD: ${usd}, AUD: ${aud}`);
+// let usd = 1200;
+// let aud = usd * 1.5;
+// console.log(`USD: ${usd}, AUD: ${aud}`);
 
-// comparison operators 
-let comparison1 = "5" == 5; // true
-let comparison2 = "5" === 5; // false
-let comparison3 = 5 != "5"; // false
-let comparison4 = 5 !== "5"; // true
-console.log(`Comparison 1: ${comparison1}, Comparison 2: ${comparison2}, Comparison 3: ${comparison3}, Comparison 4: ${comparison4}`);
+// // comparison operators 
+// let comparison1 = "5" == 5; // true
+// let comparison2 = "5" === 5; // false
+// let comparison3 = 5 != "5"; // false
+// let comparison4 = 5 !== "5"; // true
+// console.log(`Comparison 1: ${comparison1}, Comparison 2: ${comparison2}, Comparison 3: ${comparison3}, Comparison 4: ${comparison4}`);
+
+// recap of loops data types and operators
+for (let i = 1; i <= 15; i++) {
+    
+    if ( i % 5 === 0) {
+        console.log(`Asap frontend: ${i}`);
+    } 
+    else if ( i % 2 === 0) {
+        console.log(` frontend: ${i}`);
+    }
+    else {
+        console.log(`asap: ${i}`);
+    }
+}
